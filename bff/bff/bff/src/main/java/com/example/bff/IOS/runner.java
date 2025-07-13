fd2043d3c3d48640412b5f3863b4214543de98c0
Enotich Enotov <119893655+EnotichEnotow@users.noreply.github.com>
2025-07-13 17:07:34 +0300

    Реализовано автопроксирование тупых запросов
    Лишнее убрано

---
+++ bff/bff/bff/src/main/java/com/example/bff/IOS/runner.java
@@ -0,0 +1,11 @@
+package com.example.bff.IOS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class runner {
+    public static void main(String[] args) {
+        SpringApplication.run(com.example.bff.BffApplication.class, args);
+    }
+}
---
+++ bff/bff/bff/src/test/java/com/example/bff/BffApplication2Tests.java
@@ -0,0 +1,13 @@
+package com.example.bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffApplication2Tests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
